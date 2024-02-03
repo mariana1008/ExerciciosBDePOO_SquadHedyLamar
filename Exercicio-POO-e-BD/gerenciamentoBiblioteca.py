1e1dbe80141b042e7bbebc6fd8bbc9e464da5e29
saraneves2011 <nevessara2009@gmail.com>
2024-02-03 09:42:03 -0300

    criei a classe abstrata BaseBiblioteca

---
+++ Exercicio-POO-e-BD/gerenciamentoBiblioteca.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class BaseBiblioteca(ABC):
+    def __init__(self,nome,telefone,nacionalidade):
+        self._nome = nome
+        self._telefone = telefone
+        self._nacionalidade = nacionalidade
+    
+    @abstractmethod
+    def usuario(self):
+        pass    
+    
+    @abstractmethod
+    def cadastroLivro(self):
+        pass
+    
+    @abstractmethod
+    def emprestimo(self):
+        pass
+
