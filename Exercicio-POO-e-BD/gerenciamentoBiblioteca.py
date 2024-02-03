27981c7fadaa95341181745c76061e1680c72ab3
saraneves2011 <nevessara2009@gmail.com>
2024-02-03 11:31:43 -0300

    adiconei uma classe concreta

--- Exercicio-POO-e-BD/gerenciamentoBiblioteca.py
+++ Exercicio-POO-e-BD/gerenciamentoBiblioteca.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-class abstrataBiblioteca(ABC):
+class AbstrataBiblioteca(ABC):
     def __init__(self,nome,telefone,nacionalidade):
         self._nome = nome
         self._telefone = telefone
@@ -11,10 +11,29 @@ class abstrataBiblioteca(ABC):
         pass    
     
     @abstractmethod
-    def cadastroLivro(self):
+    def cadastrar_Livro(self):
         pass
     
     @abstractmethod
-    def emprestimo(self):
+    def realizar_emprestimo(self):
         pass
-
+    
+    @abstractmethod
+    def realizar_devolucao(self):
+        pass
+    
+class Biblioteca(AbstrataBiblioteca):
+    def __init__(self,nome,telefone,nacionalidade):
+        super().__init__(nome,telefone,nacionalidade)
+     
+    def usuario(self):
+        print(f"Usuário {self.nome} cadastrado com sucesso!")    
+    
+    def cadastrar_Livro(self):
+        print(f"Livro tal cadastrado com sucesso!")
+    
+    def realizar_emprestimo(self):
+        print(f"Empréstimo realizado para nome.usuario: nome.livro")
+    
+    def realizar_devolucao(self):
+        print(f"Devolução realizada por nome.usuario: nome.livro")
