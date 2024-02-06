308c9df4072a6a3f759a45efdc06f949a25b1e01
saraneves2011 <nevessara2009@gmail.com>
2024-02-05 22:43:02 -0300

    integração com banco de dados classe livro

--- classe_livro.py
+++ classe_livro.py
@@ -16,15 +16,7 @@ class Livro(Biblioteca):
         self.id = 0 
        
     def cadastrar_Livro(self):
-        cursor.execute(f'INSERT INTO livros(id,autor,titulo,editora,genero,numero_exemplar) VALUES(?,?,?,?,?,?)',{self._id},{self._autores},{self._titulo},{self._editora},{self._genero},{self._total_exemplares})
-        
-        conexao.commit()
-        
-        i = 1
-        while i <= self._total_exemplares:
-            self.id_exemplares[i] = True
-            i+=1
-        #print(f"aqui tem id {self.id_exemplares}")
+        cursor.execute('INSERT INTO livros(id, autor, titulo, editora, genero, numero_exemplar) VALUES (?, ?, ?, ?, ?, ?)',(self._id, self._autores, self._titulo, self._editora, self._genero, self._total_exemplares))
         print(f"Livro {self._titulo} cadastrado com sucesso!")
     
     def adicionar_autor(self,autor):
@@ -32,17 +24,24 @@ class Livro(Biblioteca):
         self._autores.append(autor)
     
     def realizar_emprestimo(self):
-        if self._lista_exemplares > 0:
-            self.id, boleano = self.id_exemplares.popitem()
-            #print(f"aqui tem id {self.id_exemplares}")  
-            print(f"Empréstimo realizado para o livro {self._titulo}, Exemplar {self.id}.")
-            self._lista_exemplares -= 1
-            cursor.execute('UPDATE livros SET numero_exemplar -= 1 WHERE id = ? ', (self.id))
+        if self._total_exemplares > 0:
+            print(f"Empréstimo realizado para o livro {self._titulo}")
+            self._total_exemplares -= 1
+            cursor.execute('UPDATE livros SET numero_exemplar = ? WHERE id = ?', (self._total_exemplares, self._id))
         else:
             print("não existem mais exemplares disponiveis")
             
     
     def imprimir(self):
         print(f"Titulo: {self._titulo} \nAutores: {self._autores} \nExemplares: {self._lista_exemplares}")
-        
+
+ceu_esta_em_todo_lugar = Livro(1,"Jandy","Céu está em todo lugar", "Campus","Romance",3)
+#ceu_esta_em_todo_lugar.cadastrar_Livro()
+ceu_esta_em_todo_lugar.realizar_emprestimo()
+ceu_esta_em_todo_lugar.realizar_emprestimo()
+ceu_esta_em_todo_lugar.realizar_emprestimo()
+ceu_esta_em_todo_lugar.realizar_emprestimo()
+
+
+conexao.commit()
 conexao.close()
